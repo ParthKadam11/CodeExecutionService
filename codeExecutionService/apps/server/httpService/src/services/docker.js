5f2a7eaa12f0872d82b4ec55c52ea2af750e072a
ParthKadam11 <parthkadamalt@gmail.com>
2026-05-09 23:50:02 +0530

    feat: remove unused docker service implementation

--- codeExecutionService/apps/server/httpService/src/services/docker.js
+++
@@ -1,8 +0,0 @@
-import { exec } from "node:child_process";
-import util from "util";
-const execPromise = util.promisify(exec);
-async function main() {
-    const { stdout } = await execPromise('docker ps');
-    console.log(stdout);
-}
-main();
